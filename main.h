b59cd3e21d5182e66bcc05078330ea66b6948826
hassantarrek <htarek168@gmail.com>
2023-09-08 02:48:24 -0700

    Adding AUTHORS and Header files

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#endif
